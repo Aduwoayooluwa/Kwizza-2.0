06f2bd58910e7431c60149cbac2fe5021e178a20
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2024-07-06 23:06:10 +0100

    fix: vercel prisma argumen gt

--- src/pages/quiz/Quiz.tsx
+++ src/pages/quiz/Quiz.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/utils/global"
 import { audioFiles } from "@/utils/questions";
 import QuizCard from "@/components/quiz";
 import useQuizStore from "@/store/quiz-store";
+import { ClerkProvider } from "@clerk/nextjs";
 
 function generateRandomQuestionNumbers(){
     return Math.floor(Math.random() * 100) || 20; 
@@ -16,13 +17,13 @@ export default function QuizQuestion() {
     const question = audioFiles.find((question) => question.id === generateRandomQuestionNumbers() ?? 37) || audioFiles[99];
 
     return(
-        <div>
+        <ClerkProvider>
             <QuizCard 
                 number={currentQuestionIndex + 1}
                 question={question?.word as string}
                 audioLink={question?.link as string}
                 id={question?.id as number}
             />
-        </div>
+        </ClerkProvider>
     )
 } 
